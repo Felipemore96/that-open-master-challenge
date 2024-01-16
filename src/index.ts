6d53ec8a31950660c9741a2ed08cf97c1232e4e7
Felipe Moreira <118832082+Felipemore96@users.noreply.github.com>
2024-01-16 17:07:30 -0600

    Project form added

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,20 @@ import { TeamsManager } from "./class/projectsManager"
 const teamsListUI = document.getElementById("teams-list") as HTMLElement
 const teamsManager = new TeamsManager(teamsListUI)
 
+const projectForm = document.getElementById("new-project-form")
+const cancelNewProjectBtn = document.getElementById("cancel-new-project-btn")
+const submitNewProjectBtn = document.getElementById("submit-new-project-btn")
 const newTeamBtn = document.getElementById("new-team-btn")
 const teamForm = document.getElementById("new-team-form")
 const cancelNewTeamBtn = document.getElementById("cancel-new-team-btn")
 const submitNewTeamBtn = document.getElementById("submit-new-team-btn")
+
+const newProjectBtn = document.getElementById("new-project-btn")
+if (newProjectBtn) {
+  newProjectBtn.addEventListener("click", () => {toggleModal("new-project-modal")})
+} else {
+  console.warn("New projects button was not found")
+}
 
 if (newTeamBtn) {
   newTeamBtn.addEventListener("click", () => {toggleModal("new-team-modal")})
@@ -15,11 +25,41 @@ if (newTeamBtn) {
   console.warn("New projects button was not found")
 }
 
+
 const closeErrorPopup = document.getElementById("close-error-popup")
 if (closeErrorPopup) {
   closeErrorPopup.addEventListener("click", () => {
     toggleModal("error-popup");
   });
+}
+
+if (projectForm && projectForm instanceof HTMLFormElement) {
+  submitNewProjectBtn?.addEventListener("click", (e) => {
+    e.preventDefault()
+    const formData = new FormData(projectForm)
+    const projectData: IProject = {
+      name: formData.get("name") as string,
+      description: formData.get("description") as string,
+      status: formData.get("status") as ProjectStatus,
+      userRole: formData.get("userRole") as UserRole,
+      finishDate: new Date(formData.get("finishDate") as string)
+    }
+    try {
+      const project = projectsManager.newProject(projectData)
+      projectForm.reset()
+      toggleModal("new-project-modal")
+    } catch (err) {
+      const errorMessage = document.getElementById("err") as HTMLElement
+      errorMessage.textContent = err
+      toggleModal("error-popup")
+    }
+  })
+  cancelNewProjectBtn?.addEventListener("click", () => {
+    projectForm.reset()
+    toggleModal("new-project-modal") 
+  })
+} else {
+  console.warn("The project form was not found. Check the ID!")
 }
 
 if (teamForm && teamForm instanceof HTMLFormElement) {
@@ -48,7 +88,7 @@ if (teamForm && teamForm instanceof HTMLFormElement) {
     toggleModal("new-team-modal") 
   })
 } else {
-  console.warn("The project form was not found. Check the ID!")
+  console.warn("The team form was not found. Check the ID!")
 }
 
 const closeTeamInfoPopup = document.getElementById("close-team-info-popup")
