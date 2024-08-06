588ec98e1ffc26403ffe02685f1ba0494cd185f3
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-08-06 15:01:12 -0600

    Add initial team info dialog

--- src/react-components/TeamCardTeams.tsx
+++ src/react-components/TeamCardTeams.tsx
@@ -31,6 +31,10 @@ export function TeamCardTeams(props: Props) {
     toggleModal("delete-popup");
   };
 
+  const onTeamInfoButton = () => {
+    toggleModal("info-popup");
+  };
+
   const iconConversion = (teamRole: TeamRole): string => {
     switch (teamRole) {
       case "BIM Manager":
@@ -50,42 +54,6 @@ export function TeamCardTeams(props: Props) {
 
   return (
     <div>
-      <dialog id="delete-popup">
-        <div id="error-message">
-          <section style={{ marginBottom: "20px" }}>
-            <p style={{ fontSize: "16px", margin: "10px" }}>
-              Are you sure you want to delete the team: "
-              <strong>{props.team.teamName}</strong>"?
-            </p>
-          </section>
-          <footer
-            style={{ display: "flex", justifyContent: "flex-end", gap: "10px" }}
-          >
-            <button
-              onClick={onClosePopup}
-              type="button"
-              style={{
-                padding: "10px 20px",
-                backgroundColor: "#535353",
-                color: "#fff",
-              }}
-            >
-              Close
-            </button>
-            <button
-              onClick={(e) => onDeleteTeam()}
-              type="button"
-              style={{
-                padding: "10px 20px",
-                backgroundColor: "#ff4d4d",
-                color: "#fff",
-              }}
-            >
-              Delete
-            </button>
-          </footer>
-        </div>
-      </dialog>
       <div className="team-card">
         <div>
           <div
@@ -131,7 +99,6 @@ export function TeamCardTeams(props: Props) {
             rowGap: "5px",
             cursor: "pointer",
           }}
-          onClick={onDeleteTeamButton}
         >
           <span
             className="material-icons-round"
@@ -141,6 +108,7 @@ export function TeamCardTeams(props: Props) {
               borderRadius: 10,
               fontSize: 17,
             }}
+            onClick={onDeleteTeamButton}
             onMouseOver={(e) => {
               e.currentTarget.style.backgroundColor = "#ff4d4d";
             }}
@@ -158,8 +126,9 @@ export function TeamCardTeams(props: Props) {
               borderRadius: 10,
               fontSize: 17,
             }}
+            onClick={onTeamInfoButton}
             onMouseOver={(e) => {
-              e.currentTarget.style.backgroundColor = "#ff4d4d";
+              e.currentTarget.style.backgroundColor = "#468f3f";
             }}
             onMouseOut={(e) => {
               e.currentTarget.style.backgroundColor = "#686868";
@@ -169,6 +138,45 @@ export function TeamCardTeams(props: Props) {
           </span>
         </div>
       </div>
+      <dialog id="delete-popup">
+        <div id="error-message">
+          <section style={{ marginBottom: "20px" }}>
+            <p style={{ fontSize: "16px", margin: "10px" }}>
+              Are you sure you want to delete the team: "
+              <strong>{props.team.teamName}</strong>"?
+            </p>
+          </section>
+          <footer
+            style={{ display: "flex", justifyContent: "flex-end", gap: "10px" }}
+          >
+            <button
+              onClick={onClosePopup}
+              type="button"
+              style={{
+                padding: "10px 20px",
+                backgroundColor: "#535353",
+                color: "#fff",
+              }}
+            >
+              Close
+            </button>
+            <button
+              onClick={(e) => onDeleteTeam()}
+              type="button"
+              style={{
+                padding: "10px 20px",
+                backgroundColor: "#ff4d4d",
+                color: "#fff",
+              }}
+            >
+              Delete
+            </button>
+          </footer>
+        </div>
+      </dialog>
+      <dialog id="info-popup">
+        <div id="info-message"></div>
+      </dialog>
     </div>
   );
 }
