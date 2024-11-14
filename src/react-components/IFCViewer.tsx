fd0a150647c04fde0e141152ad5cf5626e8a5850
Felipe Moreira <felipe.moreiras796@gmail.com>
2024-11-14 22:12:11 +0100

    Add initial world element 3.3.

--- src/react-components/IFCViewer.tsx
+++ src/react-components/IFCViewer.tsx
@@ -5,6 +5,7 @@ import { FragmentsGroup } from "bim-fragment";
 import { ToDoCreator } from "../bim-components/ToDoCreator";
 import { SimpleQTO } from "../bim-components/SimpleQTO";
 import { Project } from "../class/projects";
+import { cameraPosition } from "three/examples/jsm/nodes/shadernode/ShaderNodeBaseElements";
 
 interface Props {
   project: Project;
@@ -33,6 +34,31 @@ export function IFCViewer(props: Props) {
   const setViewer = () => {
     const viewer = new OBC.Components();
     const worlds = viewer.get(OBC.Worlds);
+
+    const world = worlds.create<
+      OBC.SimpleScene,
+      OBC.OrthoPerspectiveCamera,
+      OBC.SimpleRenderer
+    >();
+
+    const sceneComponent = new OBC.SimpleScene(viewer);
+    world.scene = sceneComponent;
+    world.scene.setup();
+    world.scene.three.background = null;
+
+    const viewerContainer = document.getElementById(
+      "viewer-container",
+    ) as HTMLElement;
+    const rendererComponent = new OBC.SimpleRenderer(viewer, viewerContainer);
+    world.renderer = rendererComponent;
+
+    const cameraComponent = new OBC.OrthoPerspectiveCamera(viewer);
+    world.camera = cameraComponent;
+
+    viewer.init();
+
+    world.camera.controls.setLookAt(3, 3, 3, 0, 0, 0);
+    world.camera.updateAspect();
   };
 
   React.useEffect(() => {
