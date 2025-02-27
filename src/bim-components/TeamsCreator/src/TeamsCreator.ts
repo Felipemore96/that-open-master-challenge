40364e7ef8cf0d0273d4711ff50c83bddacf37b7
Felipe Moreira <felipe.moreiras796@gmail.com>
2025-02-27 21:00:14 +0100

    Add base files

---
+++ src/bim-components/TeamsCreator/src/TeamsCreator.ts
@@ -0,0 +1,11 @@
+import * as OBC from "@thatopen/components";
+
+export class TeamsCreator extends OBC.Component {
+  static uuid = "9c51ff2a-2a8a-4b1d-8253-43854876c041";
+  enabled = true;
+
+  constructor(components: OBC.Components) {
+    super(components);
+    this.components.add(TeamsCreator.uuid, this);
+  }
+}
